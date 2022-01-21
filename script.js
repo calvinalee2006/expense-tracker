51d6606026bd482f5a96be0cbba6b5c3d7d29490
Lennart Boecken <lsboecken@gmail.com>
2022-01-21 08:54:54 -0500

    refactor

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-const expenseTable = document.getElementById('table');
-const list = document.getElementById('expense_list');
-const remove = document.getElementById('btnRemove');
+intializeExpenseTracker();
 
-table.addEventListener('submit', addItems);
-expense_list.addEventListener('click', removeItems)
-
-function addItems(e) {
+function intializeExpenseTracker() {
+  document.getElementById('inputForm').addEventListener('submit', (e) => {
     e.preventDefault();
-
-    var elements = this.elements;
-    var purchase = elements.purchase.value;
-    var location = elements.location.value;
-    var paymentType = elements.paymentType.value;
-    var date = elements.date.value;
-    var amount = elements.amount.value;
-
-    var tr = document.createElement('tr')
-
-    const purchaseTd = document.createElement('td');
-    purchaseTd.textContent = purchase;
-    tr.appendChild(purchaseTd);
-
-    const locationTd = document.createElement('td');
-    locationTd.textContent = location;
-    tr.appendChild(locationTd);
-
-    const paymentTd = document.createElement('td');
-    paymentTd.textContent = paymentType;
-    tr.appendChild(paymentTd);
-
-    const dateTd = document.createElement('td');
-    dateTd.textContent = date;
-    tr.appendChild(dateTd);
-
-    const amountTd = document.createElement('td');
-    amountTd.textContent = amount;
-    tr.appendChild(amountTd);
-
-    var removeTd = document.createElement('button');
-    removeTd.className = 'btnRemove';
-    removeTd.appendChild(document.createTextNode('X'));
-    tr.appendChild(removeTd)
-
-    list.appendChild(tr);
-    document.getElementById('table').reset();
-
+    createNewExpense();
+    document.getElementById('inputForm').reset();
+  });
+  document
+    .getElementById('expense_list')
+    .addEventListener('click', removeItems);
 }
 
+function createNewExpense() {
+  const tableRow = createTableRow();
+  const tableData = createTableData();
+  const deleteButton = createDeleteButton();
+  tableRow.append(...tableData, deleteButton);
+  appendNewExpenseToTable(tableRow);
+}
 function removeItems(e) {
-    var tr = e.target.parentElement;
-    expense_list.removeChild(tr);
-
+  const tableRow = e.target.parentElement;
+  expense_list.removeChild(tableRow);
 }
 
+function createTableRow() {
+  return document.createElement('tr');
+}
+function createTableData() {
+  const inputFormData = document.getElementById('inputForm');
+  const expenseData = [
+    inputFormData.purchase.value,
+    inputFormData.location.value,
+    inputFormData.paymentType.value,
+    inputFormData.date.value,
+    inputFormData.amount.value,
+  ];
+  const newRowElements = [];
+  for (i = 0; i < expenseData.length; i++) {
+    const newElement = document.createElement('td');
+    newElement.textContent = expenseData[i];
+    newRowElements.push(newElement);
+  }
+
+  return [...newRowElements];
+}
+
+function createDeleteButton() {
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'btnRemove';
+  deleteButton.appendChild(document.createTextNode('X'));
+  return deleteButton;
+}
+function appendNewExpenseToTable(tableRow) {
+  document.getElementById('expense_list').appendChild(tableRow);
+}
+
+// const let var -- js declarations
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types#declarations
+
+// arrays & object -- js destructuring assignment
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
